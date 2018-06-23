cda3e38f8f32d8a77a4e9a6ab5fa0373fbdb6f26
Winter Faulk <winter@faulk.me>
2018-06-23 01:24:23 -0600

    Init commit

---
+++ README.md
@@ -0,0 +1,58 @@
+## jQuery Inline Edit Plugin
+
+
+A small javascript library built on top of jQuery that allows you to easily do inline text editing using a text input, select, or textarea.
+
+---
+
+### Usage
+
+Load the plugin
+
+```
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.31/jquery.min.js"></script>
+<script src="../inline-edit.jquery.js"></script>
+```
+
+HTML
+```
+<span id="example-text"><button class="btn btn-success">Click Me</button></span>
+```
+
+JavaScript
+```
+$("#example-text").inlineEdit({
+  type: 'text',
+  onChange: function (e, text, html) {
+    // Executes when exiting inline edit mode and a change has been made
+  },
+  onEdit: function (e) {
+    // Executes when entering inline edit mode
+  },
+  onNoChange: function (e) {
+    // Executes when exiting inline edit mode but no change has been made
+  }
+});
+```
+
+---
+
+#### Properties
+
+- type (text, select, textarea) - The type of value the clicked text will turn into.
+- className - A custom class to add to the created inline edit element.
+- on (click, dblclick, etc.) - This is how the inline edit will be triggered
+- customData - Custom properties to add to the inline edit tag
+- data - Options for the select as a key:value object
+
+#### Methods
+- onChange(this, text, html) - Executes when exiting inline edit mode and a change has been made
+- onEdit(this) - Executes when entering inline edit mode
+- onNoChange(this) - Executes when exiting inline edit mode but no change has been made
+
+#### Events
+
+- inlineEdit.on.edit - Fires when exiting inline edit mode and a change has been made
+- inlineEdit.on.change - Fires when entering inline edit mode
+- inlineEdit.on.noChange - Fires when exiting inline edit mode but no change has been made
+
---
+++ inline-edit.jquery.js
@@ -0,0 +1,156 @@
+/**
+ * Created by Winter Faulk
+ * https://github.com/faulker/jqInlineEdit
+ */
+$(function ($) {
+    $.fn.inlineEdit = function (options) {
+        var running = false,
+            $oldThis,
+            newHtml,
+            newValue,
+            $this = $(this),
+            settings = $.extend({}, $.fn.inlineEdit.defaults, options);
+
+        var init = function () {
+            $this.on(settings.on, function () {
+                if (running === false) {
+                    var html;
+
+                    running = true;
+                    $oldThis = $this.clone();
+
+                    switch (settings.type.toLowerCase()) {
+                        case 'textarea':
+                            html = buildInlineTextarea($this);
+                            $this.html(html);
+                            $this.find('textarea').focusout(saveChanges).focus();
+                            break;
+                        case 'select':
+                            html = buildInlineSelect($this);
+                            $this.html(html);
+                            $this.find('select').change(saveChanges).focusout(saveChanges).focus();
+                            break;
+                        case 'text':
+                        default:
+                            html = buildInlineText($this);
+                            $this.html(html);
+                            $this.find('input').focusout(saveChanges).focus();
+                    }
+
+                    if (settings.hasOwnProperty('onEdit')) {
+                        settings.onEdit($this);
+                    }
+
+                    $this.trigger('inlineEdit.on.edit');
+                }
+            })
+        }
+
+        var saveChanges = function () {
+            switch (this.nodeName) {
+                case 'SELECT':
+                    newValue = $(this).find(':selected').text();
+                    break;
+                case 'TEXTAREA':
+                default:
+                    newValue = $(this).val();
+            }
+
+            newHtml = $oldThis.html().replace($oldThis.text(), newValue);
+
+            $(this).parent().html(newHtml);
+            running = false;
+
+            saveEvent(this);
+        }
+
+        var saveEvent = function (e) {
+            if ($oldThis.text() !== newValue) {
+                if (settings.hasOwnProperty('onChange')) {
+                    settings.onChange($this, $(e).val(), newHtml);
+                }
+
+                $this.trigger('inlineEdit.on.change');
+            }
+            else {
+                if (settings.hasOwnProperty('onNoChange')) {
+                    settings.onNoChange($this);
+                }
+
+                $this.trigger('inlineEdit.on.noChange');
+            }
+        }
+
+        var buildInlineSelect = function ($this) {
+            var output = [],
+                $value = settings.data,
+                customData = '',
+                className = (settings.className !== undefined) ? settings.className : '';
+
+            if (settings.hasOwnProperty('customData')) {
+                for (var cdKey in settings.customData) {
+                    customData = customData + ' ' + cdKey + '="' + settings.customData[cdKey] + '"';
+                }
+            }
+
+            output.push('<select class="' + className + '" ' + customData + '>');
+
+            for (var key in $value) {
+                var selected = '';
+                if ($this.text() === $value[key]) {
+                    selected = 'selected="selected"'
+                }
+                output.push('<option value="' + key + '"' + selected + '>' + $value[key] + '</option>');
+            }
+            output.push('</select>');
+
+            return output.join('\n');
+        }
+
+        var buildInlineText = function ($this) {
+            var className = (settings.className !== undefined) ? settings.className : '',
+                customData = '';
+
+            if (settings.hasOwnProperty('customData')) {
+                for (var cdKey in settings.customData) {
+                    customData = customData + ' ' + cdKey + '="' + settings.customData[cdKey] + '"';
+                }
+            }
+
+            return '<input type="text" class="' + className + '" value="' + $this.text() + '" ' + customData + '>';
+        }
+
+        var buildInlineTextarea = function ($this) {
+            var className = (settings.className !== undefined) ? settings.className : '',
+                customData = '';
+
+            if (settings.hasOwnProperty('customData')) {
+                for (var cdKey in settings.customData) {
+                    customData = customData + ' ' + cdKey + '="' + settings.customData[cdKey] + '"';
+                }
+            }
+
+            return '<textarea class="' + className + '" ' + customData + '>' + $this.text() + '</textarea>';
+        }
+
+        // Init each instance of the plugin
+        return this.each(function () {
+            if (this.inlineEdit) return;
+
+            init();
+        });
+    }
+
+    $.fn.inlineEdit.defaults = {
+        type: 'text',
+        on: 'click',
+        className: null,
+        data: null,
+        onChange: function (el, theText, theHtml) {
+        },
+        onEdit: function (el) {
+        },
+        onNoChange: function (el) {
+        }
+    };
+}(jQuery));
